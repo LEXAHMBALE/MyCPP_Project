fb6223d977d26e1df8045391d9d78be8d84f1dae
Lexah Mbale <lexahmbale5@gmail.com>
2024-05-07 23:15:51 -0700

    deleting lines of code

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -16,17 +16,12 @@ int main() {
             std::cout << "Your subscription expires within a day! Renew now and save 20%!" << std::endl;
             break;
             case 2:
-            case 3:
-            case 4:
-            case 5:
             std::cout << "Your subscription expires in " << daysUntilExpiration << " days. Renew now and save 10%!" << std::endl;
             break;
             default:
-            if (daysUntilExpiration <= 10) {
-                std::cout << "Your subscription will expire soon. Renew now!" << std::endl;
-            } else {
-                std::cout << "You have an active subscription." << std::endl;
-            }
+            
+                
+            
     }
 
     return 0;
